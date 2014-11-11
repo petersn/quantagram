f3649683cc09047df0e8554f7476f668b28ec743
Peter Schmidt-Nielsen <snp@mit.edu>
2014-11-11 08:55:56 -0500

    A whole bunch more edits

--- quantagram_render.py
+++ quantagram_render.py
@@ -7,15 +7,18 @@ begin_env = re.compile(r"\\begin\s*[{]quantagram[}]", re.MULTILINE)
 end_env = re.compile(r"\\end\s*[{]quantagram[}]", re.MULTILINE)
 quantagram_options = re.compile(r"\\quantagramoptions\s*[{]([^}]*)[}]", re.MULTILINE)
 drawing_tikz_header = r"""
-\begin{tikzpicture}[thick,rotate=%(rotate)s,xscale=1,yscale=%(yscale)s]
-\tikzstyle{operator} = [draw,fill=white,minimum size=1.5em] 
+\begin{tikzpicture}[thick,rotate=%(rotate)s,xscale=1,yscale=%(yscale)s,cross/.style={path picture={ 
+  \draw[black]
+(path picture bounding box.south) -- (path picture bounding box.north) (path picture bounding box.west) -- (path picture bounding box.east);
+}},fredkinex/.style={path picture={ 
+  \draw[black]
+(path picture bounding box.south east) -- (path picture bounding box.north west) (path picture bounding box.south west) -- (path picture bounding box.north east);
+}}]
+\tikzstyle{operator} = [draw,fill=blue!10,minimum size=1.5em] 
 \tikzstyle{phase} = [draw,fill,shape=circle,minimum size=5pt,inner sep=0pt]
-\tikzstyle{surround} = [fill=blue!10,thick,draw=black,rounded corners=2mm]
+\tikzstyle{surround} = [fill=white,thick,draw=black,rounded corners=2mm]
 \matrix[row sep=%(row-sep)s, column sep=%(column-sep)s] (circuit) {
 """
-
-def lerp(a, b, coef):
-	return a[0]*(1-coef) + b[0]*coef, a[1]*(1-coef) + b[1]*coef
 
 def error(s):
 	print "="*len(s)
@@ -31,14 +34,14 @@ def remove_latex_comments(s):
 global_config = {
 	"rotate": 0,
 	"flip": 0,
-	"photon-frequency": 1.0,
-	"photon-amplitude": 1.0,
-	"row-sep": "0.4cm",
-	"column-sep": "0.8cm",
+	"bubble": 0,
+	"row-sep": "3mm",
+	"column-sep": "5mm",
+	"input-length": "3mm",
+	"output-length": "3mm",
 }
-
-float_options = ["flip", "rotate", "photon-frequency", "photon-amplitude"]
-string_options = ["row-sep", "column-sep"]
+float_options = ["flip", "rotate", "bubble"]
+string_options = ["row-sep", "column-sep", "input-length", "output-length"]
 
 def global_interpret(opt):
 	print "="*5, "\\quantagramoptions{%s}" % opt
@@ -82,27 +85,162 @@ def extract_envs(s):
 		s = s[match.end():]
 	return envs
 
-class Drawing:
-	def __init__(self):
-		pass
+class Node:
+	def __init__(self, x, bit, type_of, code):
+		self.x, self.bit, self.type_of, self.code = x, bit, type_of, code
 
 	def to_code(self):
+		if self.type_of == "node":
+			s = r"\node"
+		elif self.type_of == "op":
+			s = r"\node[operator]"
+		elif self.type_of == "dot":
+			s = r"\node[phase]"
+		elif self.type_of == "plus":
+			s = r"\node[draw,circle,cross]"
+		elif self.type_of == "x":
+			s = r"\node[fredkinex]"
+		else: assert False
+		s += " (xy%ib%i) {\ensuremath{%s}};" % (self.x, self.bit, self.code)
+		return s
+
+class Line:
+	def __init__(self, x1, bit1, x2, bit2):
+		self.x1, self.bit1, self.x2, self.bit2 = x1, bit1, x2, bit2
+
+class BracketDecoration:
+	def __init__(self, low, high, code, direction):
+		self.low, self.high, self.code, self.direction = low, high, code, direction
+
+	def to_code(self, d):
+		y_shift = "0.5mm" if self.low != self.high else "2mm"
+		x_shift = "1mm" if self.direction == "right" else "-1mm"
+		x = d.maximum_x if self.direction == "right" else 1
+		do_mirror = "" if self.direction == "right" else ",mirror"
+		node_name = d.new_node_name()
+		return (r"""\draw[decorate,decoration={brace%s},thick]
+($(xy%ib%i.east)+(%s,%s)$)
+to node[midway,%s] (%s) {$\displaystyle %s$}
+($(xy%ib%i.east)+(%s,-%s)$);
+""" % (do_mirror, x, self.low, x_shift, y_shift, self.direction, node_name, self.code, x, self.high, x_shift, y_shift), [node_name])
+
+class Drawing:
+	def __init__(self):
+		self.nodes = []
+		self.lines = []
+		self.decos = []
+		self.node_name_index = 0
+
+	def new_node_name(self):
+		self.node_name_index += 1
+		return "dynnode%i" % self.node_name_index
+
+	def add_node(self, n):
+		self.nodes.append(n)
+
+	def add_line(self, l):
+		self.lines.append(l)
+
+	def add_deco(self, d):
+		self.decos.append(d)
+
+	def compute(self):
+#		self.maximum_x = max(n.x for n in self.nodes)
+		self.row_count = 0
+		if self.nodes:
+			self.row_count = max(self.row_count, max(n.bit for n in self.nodes))
+		if self.decos:
+			self.row_count = max(self.row_count, max(d.high for d in self.decos))
+
+	def to_code(self):
+		self.compute()
 		s = drawing_tikz_header % global_config
+		# Draw the matrix row by row.
+		for bit in xrange(1, self.row_count+1):
+			for x in xrange(1, self.maximum_x+1):
+				for node in self.nodes:
+					if node.bit != bit or node.x != x:
+						continue
+					# Now we render the node.
+					s += node.to_code()
+					break
+				else:
+					# Make sure there's at least a place keeper node.
+					s += "\\coordinate (xy%ib%i);" % (x, bit)
+				s += " &\n"
+			s += "\\coordinate (end%i);\\\\\n" % bit
+		s += "};\n"
+		# Now, add on doodads in the background layer.
+		fit_list = ["(xy%ib%i)" % (x, bit) for x in xrange(1, self.maximum_x+1) for bit in xrange(1, self.row_count+1)]
+		s += r"\begin{pgfonlayer}{qg2}"
+		# Draw the horizontal lines.
+		for bit in xrange(1, self.row_count+1):
+			s += "\\draw[thick] (xy1b%i) -- (xy%ib%i);\n" % (bit, self.maximum_x, bit)
+		# Draw the interaction lines.
+		for line in self.lines:
+			s += "\\draw[thick] (xy%ib%i.center) -- (xy%ib%i.center);\n" % (line.x1, line.bit1, line.x2, line.bit2)
+		for deco in self.decos:
+			new_code, new_nodes = deco.to_code(self)
+			s += new_code
+			for n in new_nodes:
+				fit_list.append("(%s)" % n)
+		s += "\\end{pgfonlayer}\n"
+		if global_config["bubble"]:
+			s += "\\begin{pgfonlayer}{qg1}\n"
+			s += "\\node[surround] (supernode) [fit = %s] {};"% " ".join(fit_list)
+			s += "\\end{pgfonlayer}\n" 
 		s += "\\end{tikzpicture}\n"
 		return s
 
 def compile_quantagram_code(code):
 	d = Drawing()
-	code = code.split("\n")
+	code = code.replace(";", "\n").split("\n")
+	x = 1
 	for line in code:
 		line = line.split("%")[0].strip()
 		if not line: continue
 		line = line.split(" ")
 		cmd, args = line[0], line[1:]
-		if cmd == "foo":
-			print "Foo:", args
+		if cmd in ("node", "op"):
+			# Get the coordinate of the node.
+			bit = int(args[0])
+			# All remaining text (rejoined) is the code.
+			code = " ".join(args[1:])
+			d.add_node(Node(x, bit, cmd, code))
+		elif cmd == "cnot":
+			control = int(args[0])
+			flip = int(args[1])
+			d.add_node(Node(x, control, "dot", ""))
+			d.add_node(Node(x, flip, "plus", ""))
+			d.add_line(Line(x, control, x, flip))
+		elif cmd == "toffoli":
+			c1 = int(args[0])
+			c2 = int(args[1])
+			flip = int(args[2])
+			d.add_node(Node(x, c1, "dot", ""))
+			d.add_node(Node(x, c2, "dot", ""))
+			d.add_node(Node(x, flip, "plus", ""))
+			d.add_line(Line(x, c1, x, c2))
+			d.add_line(Line(x, c2, x, flip))
+		elif cmd == "fredkin":
+			control = int(args[0])
+			swap1 = int(args[1])
+			swap2 = int(args[2])
+			d.add_node(Node(x, control, "dot", ""))
+			d.add_node(Node(x, swap1, "x", ""))
+			d.add_node(Node(x, swap2, "x", ""))
+			d.add_line(Line(x, control, x, swap1))
+			d.add_line(Line(x, swap1, x, swap2))
+		elif cmd == "next":
+			x += 1
+		elif cmd in ("start-def", "end-def"):
+			low, high = int(args[0]), int(args[1])
+			code = " ".join(args[2:])
+			direction = "left" if cmd == "start-def" else "right"
+			d.add_deco(BracketDecoration(low, high, code, direction=direction))
 		else:
 			error("Invalid command: %s" % cmd)
+	d.maximum_x = x
 	# Do some processing on global_config.
 	global_config["yscale"] = -1 if global_config["flip"] else 1
 	return d.to_code()
